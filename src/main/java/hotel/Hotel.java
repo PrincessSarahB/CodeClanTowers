446e3822b394460dd6e523451d833d36385b48fb
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-04 16:04:07 +0100

    hotel has empty room array lists

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -1,6 +1,33 @@
 package hotel;
 
+import rooms.Bedroom;
+import rooms.ConferenceRoom;
+import rooms.DiningRoom;
+import rooms.RoomType;
+
+import java.util.ArrayList;
+
 public class Hotel {
 
-//    array list of bedrooms, conference rooms and other rooms?
+    private ArrayList<ConferenceRoom> conferenceRooms;
+    private ArrayList<RoomType> rooms;
+    private ArrayList<DiningRoom> diningrooms;
+
+    public Hotel() {
+        this.conferenceRooms = new ArrayList<>();
+        this.rooms = new ArrayList<>();
+        this.diningrooms = new ArrayList<>();
+    }
+
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return conferenceRooms;
+    }
+
+    public ArrayList<RoomType> getRooms() {
+        return rooms;
+    }
+
+    public ArrayList<DiningRoom> getDiningrooms() {
+        return diningrooms;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,12 +1,37 @@
 import hotel.Hotel;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
 
     Hotel hotel;
 
+
+
     @Before
     public void before(){
         hotel = new Hotel();
     }
+
+    @Test
+
+    public void hasBedrooms(){
+        assertEquals(0, hotel.getRooms().size());
+    }
+
+    @Test
+
+    public void hasConferenceRooms(){
+        assertEquals(0, hotel.getConferenceRooms().size());
+    }
+
+    @Test
+
+    public void hasDiningRooms(){
+        assertEquals(0, hotel.getDiningrooms().size());
+    }
+
+
 }
