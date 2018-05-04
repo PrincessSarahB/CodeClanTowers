8f5cc29875acdcd4e499b56a532135b1a7f4ad58
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-04 15:28:10 +0100

    conference room tests passing

--- src/main/java/rooms/ConferenceRoom.java
+++ src/main/java/rooms/ConferenceRoom.java
@@ -1,4 +1,21 @@
 package rooms;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+
+    private String name;
+    private Double rate;
+
+    public ConferenceRoom(int capacity, String name, Double rate) {
+        super(capacity);
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+import rooms.ConferenceRoom;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conference1;
+
+    @Before
+    public void before(){
+        conference1 = new ConferenceRoom(100, "Alpha", 300.00);
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(100, conference1.getCapacity());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Alpha", conference1.getName());
+    }
+
+    @Test
+    public void hasRate(){
+        assertEquals(300.00, conference1.getRate(), 0.01);
+    }
 }
